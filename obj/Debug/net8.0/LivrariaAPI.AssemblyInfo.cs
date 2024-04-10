edb9b3af67008d4d0452d3902e0e75b80867468c
bruna <bruna.cps003@gmail.com>
2024-04-10 17:49:11 -0300

    add: arquivo .gitignore

--- obj/Debug/net8.0/LivrariaAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/LivrariaAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LivrariaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3288f81df77190a1ed5c2d8ab456a4f4c45db1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf11e2abe42e6271ed127b884a038efefcf59613")]
 [assembly: System.Reflection.AssemblyProductAttribute("LivrariaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LivrariaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
